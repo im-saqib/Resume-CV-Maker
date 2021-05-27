4fa17d10e08841cd16b82340c0d3ec12c80417e5
Saqib <saqi_mm@hotmail.com>
2021-05-27 19:22:07 +0500

    Folder Structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
+import HomePage from "./components/Welcome";
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <h1>CV Maker</h1>
+        <HomePage />
       </div>
     );
   }
